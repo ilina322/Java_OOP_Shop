9f84293e40399268fd84f87e1c9824ab45a70ee3
Teodor Kostadinov <ilina.pavllova@gmail.com>
2021-06-09 21:13:30 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+public class Main {
+    public static void main(String[] args) {
+        testNadq();
+        testDeni();
+        testToshi();
+    }
+
+    /**
+     * If you want to test functionalities, please write all the test code
+     * ONLY IN YOUR test method!
+     * DO NOT touch main and DO NOT touch others' methods
+     * Please delete all code from your method before committing your code
+     */
+    public static void testNadq(){
+
+    }
+
+    public static void testDeni(){
+
+    }
+
+    public static void testToshi(){
+
+    }
+}
